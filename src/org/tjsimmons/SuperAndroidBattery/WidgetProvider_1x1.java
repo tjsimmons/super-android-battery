9e3be32b516606bc69f22f00e84b6ff35a54dcb1
T.J. Simmons <tj@tjsimmons.org>
2012-04-29 09:31:23 -0500

    well, changed the way things are working around here

---
+++ src/org/tjsimmons/SuperAndroidBattery/WidgetProvider_1x1.java
@@ -0,0 +1,21 @@
+package org.tjsimmons.SuperAndroidBattery;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class WidgetProvider_1x1 extends BaseWidgetProvider {
+	//RemoteViews views;
+	
+	@Override
+	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+		for (int i = 0; i < appWidgetIds.length; i++) {
+			//views = new RemoteViews(context.getPackageName(), R.layout.widget_2x1); 
+			
+			Intent intent = new Intent(context, BatteryUpdateService.class);
+			
+			context.startService(intent);
+		}
+	}
+}
