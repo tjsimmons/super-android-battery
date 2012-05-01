48df44b2536824381a18518de143f8d2a8fede5d
T.J. Simmons <tj@tjsimmons.org>
2012-05-01 14:26:56 -0500

    way way more efficient

--- src/org/tjsimmons/SuperAndroidBattery/WidgetProvider_2x1.java
+++ src/org/tjsimmons/SuperAndroidBattery/WidgetProvider_2x1.java
@@ -5,16 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 
 public class WidgetProvider_2x1 extends BaseWidgetProvider {
-	//RemoteViews views;
-	
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-		for (int i = 0; i < appWidgetIds.length; i++) {
-			//views = new RemoteViews(context.getPackageName(), R.layout.widget_2x1); 
-			
-			Intent intent = new Intent(context, BatteryUpdateService.class);
-			
-			context.startService(intent);
-		}
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
 	}
 }
