ea5ff63adab9fe9ca773ab19653372b20a72a22b
T.J. Simmons <tj@tjsimmons.org>
2012-05-01 14:49:52 -0500

    merged changes from fixes

--- src/org/tjsimmons/SuperAndroidBattery/WidgetProvider_2x1.java
+++ src/org/tjsimmons/SuperAndroidBattery/WidgetProvider_2x1.java
@@ -2,7 +2,6 @@ package org.tjsimmons.SuperAndroidBattery;
 
 import android.appwidget.AppWidgetManager;
 import android.content.Context;
-import android.content.Intent;
 
 public class WidgetProvider_2x1 extends BaseWidgetProvider {
 	@Override
